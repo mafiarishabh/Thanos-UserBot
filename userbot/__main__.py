2497bc541231200984476bceb55b2b613211f8b0
RISHABH ANANAD <94896142+thanosuser@users.noreply.github.com>
2021-11-24 15:40:54 +0530

    Add files via upload

---
+++ userbot/__main__.py
@@ -0,0 +1,60 @@
+from userbot import bot
+from sys import argv
+import sys
+from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
+import os
+from telethon import TelegramClient
+from var import Var
+from userbot.Config import Config
+from userbot.utils import load_module
+from userbot import LOAD_PLUG, LOGS, REBELversion
+from pathlib import Path
+import asyncio
+import telethon.utils
+
+os.system("pip install -U telethon")
+
+async def add_bot(bot_token):
+    await bot.start(bot_token)
+    bot.me = await bot.get_me() 
+    bot.uid = telethon.utils.get_peer_id(bot.me)
+
+
+
+if len(argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.tgbot = None
+    if Var.TG_BOT_USER_NAME_BF_HER is not None:
+        print("Initiating Inline Bot")
+        # ForTheGreatrerGood of beautification
+        bot.tgbot = TelegramClient(
+            "TG_BOT_TOKEN",
+            api_id=Var.APP_ID,
+            api_hash=Var.API_HASH
+        ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
+        print("Initialisation finished with no errors")
+        print("🔰ՏTᗩᖇT THANOS🔰")
+        bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
+        print("⚡THANOS ՏTᗩᖇTᑌᑭ ᑕOᗰᑭᒪᗴTᗴᗪ⚡")
+    else:
+        bot.start()
+
+
+import glob
+path = 'userbot/plugins/*.py'
+files = glob.glob(path)
+for name in files:
+    with open(name) as f:
+        path1 = Path(f.name)
+        shortname = path1.stem
+        load_module(shortname.replace(".py", ""))
+
+import userbot._core
+
+print(f"""Hello sir i am THANOS!! THANOS VERSION :- {REBELversion} YOUR THANOS IS READY! FOR CHECK YOUR BOT WORKING OR NOT PLEASE TYPE (.thanos/.ping) ENJOY YOUR BOT! JOIN FOR MORE FUTURE UPDATES @thanosBot_chat .""")
+
+if len(argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.run_until_disconnected()
