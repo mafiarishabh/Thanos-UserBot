9d9fb8772e11846f85d2a971a7adf232b1ae7a76
RISHABH ANANAD <94896142+thanosuser@users.noreply.github.com>
2021-11-28 13:42:12 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -32,7 +32,7 @@ pm_caption = "  __**🔥🔥ＴＨΛＮ♢Ｓ  ＩＳ  ΛＬＩＶΣ🔥🔥**__
 
 pm_caption += f"**━━━━━━━━━━━━━━━━━━━━━━━━━━━**\n\n"
 pm_caption += (
-    f"                💞BOSS💞\n      **『😈[{DEFAULTUSER}](tg://user?id={REBEL})😈』**\n\n"
+    f"                💞BOSS💞\n      **『😈[{DEFAULTUSER}](tg://user?id={THANOS})😈』**\n\n"
 )
 pm_caption += f"╭────────────\n"
 pm_caption += f"┣•➳➠ `ＴΞＬΣＴＨ♢Ｎ:` `{version.__version__}` \n"
