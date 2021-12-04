96fc78721b86ef54a10ba0a05206ccc3a086cd90
RISHABH ANANAD <94896142+thanosuser@users.noreply.github.com>
2021-12-04 14:52:11 +0530

    Update rishabh.py

--- userbot/plugins/rishabh.py
+++ userbot/plugins/rishabh.py
@@ -1,4 +1,4 @@
-# PLUGIN MADE BY @REBEL_IS_OP FOR REBELBOT
+# PLUGIN MADE BY @MAFIARISHABH FOR THANOSBOT 
 # KEEP CREDITS ELSE GAY
 
 import random, re
@@ -10,3 +10,6 @@ from telethon import events
 async def _(event):
      if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
         await event.edit("""SUN TUU MERA 😍😍JAAN😘😘 HAA MANA PYAR 🌹🌹KIYA HAA KOI MAZAK NAHI. AJJ NIGHT 💓 MAA TUU BHUT YAAD😔😔 AAA RAHA THA . TU BASS MERA 💞💕💗 MERA JAAN HAA BASS MERA💖. I LOVE 💝YOU RISHABH😘😘""")
+
+CmdHelp("RISHABH").add_command(
+  "rishabh", "<tag thanosbot owner>", "send thanos bot owner and enjoy."
