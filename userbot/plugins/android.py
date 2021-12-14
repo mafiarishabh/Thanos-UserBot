c8f05f92c9a7c690748b788246733d8d5a06c0c8
RISHABH ANANAD <94896142+thanosuser@users.noreply.github.com>
2021-12-14 21:06:57 +0530

    Update android.py

--- userbot/plugins/android.py
+++ userbot/plugins/android.py
@@ -13,7 +13,7 @@ from requests import get
 from userbot.cmdhelp import CmdHelp
 
 from userbot import *
-from REBELBOT.utils import *
+from THANOSBOT.utils import *
 
 GITHUB = "https://github.com"
 DEVICES_DATA = (
